637f4266a23268c42c9b810c60990774cd8a302a
Harshiladze Levan <leeone95@yandex.ru>
2025-11-19 22:15:42 +0300

    add exception handler v0.3 Недоделано

--- auth-service/build.gradle.kts
+++ auth-service/build.gradle.kts
@@ -34,7 +34,6 @@ checkstyle {
 }
 
 dependencies {
-	implementation(":global-exceptions")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("org.springframework.boot:spring-boot-starter-web")
