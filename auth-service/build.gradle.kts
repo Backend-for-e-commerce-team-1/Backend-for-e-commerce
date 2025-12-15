dddd09f79352c878f4c4f3067b080d4b12bfdf02
Plastinin-Igor <igor-plastinin@yandex.ru>
2025-12-15 15:03:46 +0300

    Настроить структурированное логирование и базовый мониторинг микросервисов

--- auth-service/build.gradle.kts
+++ auth-service/build.gradle.kts
@@ -58,6 +58,7 @@ dependencies {
 	implementation("io.opentelemetry:opentelemetry-exporter-logging")
 	implementation("io.micrometer:micrometer-tracing-bridge-otel")
 	implementation("io.micrometer:micrometer-observation")
+    implementation("net.logstash.logback:logstash-logback-encoder:7.4")
 
   implementation("org.liquibase:liquibase-core")
 	implementation(project(":security-lib"))
