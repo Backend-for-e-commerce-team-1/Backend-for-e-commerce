971be7b336a8ee272675cb172249d4f880c24186
Svetlana Rozanova <rozanovasa@yandex.ru>
2025-12-25 18:12:54 +0300

    fixed a typo

--- cart-service/src/main/java/ru/practikum/masters/cartservice/CartServiceApplication.java
+++ cart-service/src/main/java/ru/practikum/masters/cartservice/CartServiceApplication.java
@@ -6,7 +6,8 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class CartServiceApplication {
-    static void main(String[] args) {
+
+    public static void main(String[] args) {
         SpringApplication.run(CartServiceApplication.class, args);
     }
 }
